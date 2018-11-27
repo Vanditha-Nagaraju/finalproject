70305d2b88caa48da0a9a8c7756d271cc163ca4b
vanditha <vn245@njit.edu>
2018-11-27 15:07:25 -0500

    Feature : Form added to add questions, view, edit and delete them

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Questions
-                        <a class="btn btn-primary float-right" href="#">
+                        <a class="btn btn-primary float-right" href="{{ route('questions.create') }}">
                             Create a Question
                         </a>
 
@@ -37,10 +37,10 @@
                                         </div>
                                     </div>
                                 @empty
-                                    There are no questions to view, you can  create a question.
+                                     <a class="btn btn-primary " href="{{ route('questions.create')}}">
+                                         There are no questions to view, you can  create a question.
+                                     </a>
                                 @endforelse
-
-
                             </div>
 
                         </div>
